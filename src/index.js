a95176c6f7af36cd44875eb3160ef48b9f438373
Andrii Kulba <avk.0149344@gmail.com>
2020-12-17 21:55:08 +0200

    17.12 up

--- src/index.js
+++ src/index.js
@@ -1,26 +1,38 @@
+import debounced from 'lodash.debounce';
 import './styles.css';
+import countryCardTpl from './templates/country-card.hbs';
 import countriesCardTpl from './templates/countries-card.hbs';
 import API from './js/fetchCountries';
 import getRefs from './js/get-refs';
 
 const refs = getRefs();
 
-refs.searchForm.addEventListener('submit', onSearch);
+refs.searchForm.addEventListener('input', onSearch);
 
 function onSearch(e) {
   e.preventDefault();
 
   const form = e.currentTarget;
   const searchQuery = form.elements.query.value;
-
+  console.log(searchQuery);
   API.fetchCountries(searchQuery)
-    .then(console.log)
+    .then(renderCountriesCard)
     .catch(onFetchError)
     .finally(() => form.reset());
 }
 
 function renderCountriesCard(name) {
-  const markup = countriesCardTpl(name[0]);
+  const countElementsObj = Object.keys(name).length;
+  console.log(countElementsObj);
+  console.log(name);
+
+  if (countElementsObj !== 1) {
+    const markup1 = countriesCardTpl(name);
+
+    refs.cardContainer.innerHTML = markup1;
+  }
+  const markup = countryCardTpl(name[0]);
+
   refs.cardContainer.innerHTML = markup;
 }
 
