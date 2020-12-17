187784c2ddd0612213b459912b60d0bd2cc8fbee
AndriiKulba <avk.0149344@gmail.com>
2020-12-17 13:51:55 +0200

    17.12

--- src/index.js
+++ src/index.js
@@ -1,2 +1,42 @@
 import './styles.css';
-import API from './fetchCountries';
+import pokemonCardTpl from './templates/countries-card.hbs';
+import API from './js/fetchCountries';
+import getRefs from './js/get-refs';
+
+const refs = getRefs();
+
+refs.searchForm.addEventListener('submit', onSearch);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  const form = e.currentTarget;
+  const searchQuery = form.elements.query.value;
+
+  API.fetchCountries(searchQuery)
+    .then(console.log)
+    .catch(onFetchError)
+    .finally(() => form.reset());
+}
+
+function renderPokemonCard(pokemon) {
+  const markup = pokemonCardTpl(pokemon);
+  refs.cardContainer.innerHTML = markup;
+}
+
+function onFetchError(error) {
+  alert('Упс, что-то пошло не так и мы не нашли вашего покемона!');
+}
+
+// =========================================
+
+// const url = 'https://newsapi.org/v2/everything?q=cars';
+// const options = {
+//   headers: {
+//     Authorization: '4330ebfabc654a6992c2aa792f3173a3',
+//   },
+// };
+
+// fetch(url, options)
+//   .then(r => r.json())
+//   .then(console.log);
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,8 @@
+// function fetchCountries(searchQuery)
+const BASE_URL = 'https://restcountries.eu/rest/v2';
+
+function fetchCountries(name) {
+  return fetch(`${BASE_URL}/name/${name}`).then(response => response.json());
+}
+
+export default { fetchCountries };
