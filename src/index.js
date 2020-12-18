75449caef7ed3085b60be4cdb2ada7a7e4b39caa
AndriiKulba <avk.0149344@gmail.com>
2020-12-18 15:13:12 +0200

    18.12

--- src/index.js
+++ src/index.js
@@ -1,44 +1,56 @@
-import debounced from 'lodash.debounce';
 import './styles.css';
 import countryCardTpl from './templates/country-card.hbs';
 import countriesCardTpl from './templates/countries-card.hbs';
 import API from './js/fetchCountries';
 import getRefs from './js/get-refs';
 
+const debounce = require('lodash.debounce');
+
+import { alert, defaultModules } from '@pnotify/core';
+import * as PNotifyMobile from '@pnotify/mobile';
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/Material.css';
+
+defaultModules.set(PNotifyMobile, {});
+
+alert({
+  text: 'Такой страны не найдено, введите более точный запрос',
+});
+
 const refs = getRefs();
-
 refs.searchForm.addEventListener('input', onSearch);
-
+refs.searchForm.addEventListener('submit', onSearch);
 function onSearch(e) {
   e.preventDefault();
-
+  console.log(e);
   const form = e.currentTarget;
   const searchQuery = form.elements.query.value;
-  console.log(searchQuery);
   API.fetchCountries(searchQuery)
     .then(renderCountriesCard)
     .catch(onFetchError)
-    .finally(() => form.reset());
+    .finally(() => form);
 }
-
+// .finally(() => form.reset());
 function renderCountriesCard(name) {
-  const countElementsObj = Object.keys(name).length;
-  console.log(countElementsObj);
-  console.log(name);
+  const countElementsObj = Object.values(name).length;
+  console.log(Object.values(name));
 
-  if (countElementsObj !== 1) {
-    const markup1 = countriesCardTpl(name);
-
-    refs.cardContainer.innerHTML = markup1;
+  if (countElementsObj === 1) {
+    const markup = countryCardTpl(name);
+    refs.cardContainer.innerHTML = markup;
+  } else if ((countElementsObj > 2) & (countElementsObj < 10)) {
+    const markup = countriesCardTpl(name);
+    refs.cardContainer.innerHTML = markup;
+    const countriesItem = document.querySelectorAll('.list-group-item');
+    console.log(countriesItem);
   }
-  const markup = countryCardTpl(name[0]);
-
-  refs.cardContainer.innerHTML = markup;
 }
 
 function onFetchError(error) {
   alert('Упс, что-то пошло не так и мы не нашли вашего покемона!');
 }
+
+const _ = require('lodash');
 
 // =========================================
 
